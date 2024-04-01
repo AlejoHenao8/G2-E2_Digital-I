4d8b76f662dace2c42712fc9c83ccd1efe72fa8b
Angel26c <160276156+Angel26c@users.noreply.github.com>
2024-04-01 14:02:49 -0500

    Add files via upload

--- Laboratorio003/src/Parte1/seg7.v
+++ Laboratorio003/src/Parte1/seg7.v
@@ -1,8 +1,8 @@
 module seg7 (BCD, SSeg, an);
 
-  input [2:0] BCD;
+  input [3:0] BCD;
   output reg [0:6] SSeg;
-  output [2:0] an;
+  output [3:0] an;
   
   assign an = 4'b1110;
 
@@ -11,14 +11,16 @@ always @ ( * ) begin
   case (BCD)
 
                          // abcdefg
-         3'b000: SSeg = 7'b0000001; // "0"  
-	 3'b001: SSeg = 7'b1001111; // "1" 
-	 3'b010: SSeg = 7'b0010010; // "2" 
-	 3'b011: SSeg = 7'b0000110; // "3" 
-	 3'b100: SSeg = 7'b1001100; // "4" 
-	 3'b101: SSeg = 7'b0100100; // "5" 
-	 3'b110: SSeg = 7'b0100000; // "6" 
-	 3'b111: SSeg = 7'b0001111; // "7" 
+         4'b0000: SSeg = 7'b0000001; // "0"  
+	 4'b0001: SSeg = 7'b1001111; // "1" 
+	 4'b0010: SSeg = 7'b0010010; // "2" 
+	 4'b0011: SSeg = 7'b0000110; // "3" 
+	 4'b0100: SSeg = 7'b1001100; // "4" 
+	 4'b0101: SSeg = 7'b0100100; // "5" 
+	 4'b0110: SSeg = 7'b0100000; // "6" 
+	 4'b0111: SSeg = 7'b0001111; // "7" 
+	 4'b1000: SSeg = 7'b0000000; // "8"  
+	 4'b1001: SSeg = 7'b0000100; // "9" 
     default:
     SSeg = 0;
   endcase
